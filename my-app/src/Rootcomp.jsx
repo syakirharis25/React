c83ab99dec99b99c3e2d4c9c47faa90f7269f990
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2020-03-10 19:23:23 +0800

    modified:   my-app/src/Rootcomp.jsx

--- my-app/src/Rootcomp.jsx
+++ my-app/src/Rootcomp.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
 
 class RootComp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isValid: "very false"
+    };
+  }
+
   render() {
-    {
-      this.props.item;
-    }
+    return (
+      <div>
+        <h4>This is the fact and it is {this.state.isValid}</h4>
+      </div>
+    );
   }
 }
 
