763b2a229081b8ac5290b900b5ab2e0dc358e0fb
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2020-03-10 18:46:23 +0800

    modified:   my-app/src/Rootcomp.jsx

--- my-app/src/Rootcomp.jsx
+++ my-app/src/Rootcomp.jsx
@@ -1,36 +1,11 @@
-import React from "react";
-import "./index.css";
-import Item from "./Items";
+import React, { Component } from "react";
 
-function RootComp() {
-  return (
-    <div>
-      <Item
-        image="https://www-konga-com-res.cloudinary.com/w_auto,f_auto,fl_lossy,dpr_auto,q_auto/media/catalog/product/F/W/_1576485754.jpg"
-        name="Fairly Used Laptop"
-        price="400"
-        description="blue seal and silver keypads"
-      />
-      <Item
-        image="https://www-konga-com-res.cloudinary.com/w_auto,f_auto,fl_lossy,dpr_auto,q_auto/media/catalog/product/F/W/_1576485754.jpg"
-        name="Fairly Used Laptop"
-        price="400"
-        description="blue seal and silver keypads"
-      />
-      <Item
-        image="https://www-konga-com-res.cloudinary.com/w_auto,f_auto,fl_lossy,dpr_auto,q_auto/media/catalog/product/F/W/_1576485754.jpg"
-        name="Fairly Used Laptop"
-        price="400"
-        description="blue seal and silver keypads"
-      />
-      <Item
-        image="https://www-konga-com-res.cloudinary.com/w_auto,f_auto,fl_lossy,dpr_auto,q_auto/media/catalog/product/F/W/_1576485754.jpg"
-        name="Fairly Used Laptop"
-        price="400"
-        description="blue seal and silver keypads"
-      />
-    </div>
-  );
+class RootComp extends Component {
+  render() {
+    {
+      this.props.item;
+    }
+  }
 }
 
 export default RootComp;
