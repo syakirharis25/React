2bc0aeda953aa1a9ab07a4b12876c972496ffbf6
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2020-03-10 13:33:10 +0800

    new file:   my-app/src/Items.jsx         modified:   my-app/src/Rootcomp.jsx         modified:   my-app/src/index.css

--- my-app/src/Rootcomp.jsx
+++ my-app/src/Rootcomp.jsx
@@ -1,16 +1,34 @@
 import React from "react";
-import About from "./About";
-import Header from "./Header";
-import Footer from "./Footer";
-import Banner from "./Banner";
+import "./index.css";
+import Item from "./Items";
 
 function RootComp() {
   return (
     <div>
-      <Header />
-      <Banner />
-      <About />
-      <Footer />
+      <Item
+        image="https://www-konga-com-res.cloudinary.com/w_auto,f_auto,fl_lossy,dpr_auto,q_auto/media/catalog/product/F/W/_1576485754.jpg"
+        name="Fairly Used Laptop"
+        price="400"
+        description="blue seal and silver keypads"
+      />
+      <Item
+        image="https://www-konga-com-res.cloudinary.com/w_auto,f_auto,fl_lossy,dpr_auto,q_auto/media/catalog/product/F/W/_1576485754.jpg"
+        name="Fairly Used Laptop"
+        price="400"
+        description="blue seal and silver keypads"
+      />
+      <Item
+        image="https://www-konga-com-res.cloudinary.com/w_auto,f_auto,fl_lossy,dpr_auto,q_auto/media/catalog/product/F/W/_1576485754.jpg"
+        name="Fairly Used Laptop"
+        price="400"
+        description="blue seal and silver keypads"
+      />
+      <Item
+        image="https://www-konga-com-res.cloudinary.com/w_auto,f_auto,fl_lossy,dpr_auto,q_auto/media/catalog/product/F/W/_1576485754.jpg"
+        name="Fairly Used Laptop"
+        price="400"
+        description="blue seal and silver keypads"
+      />
     </div>
   );
 }
