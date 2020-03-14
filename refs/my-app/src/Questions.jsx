cd9b3135bffdc614362064c50cb195bd093d95a8
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2020-03-14 11:07:35 +0800

    modified:   my-app/src/Form.jsx         modified:   my-app/src/MyApp.jsx         new file:   refs/my-app/.gitignore         new file:   refs/my-app/README.md         new file:   refs/my-app/package-lock.json         new file:   refs/my-app/package.json         new file:   refs/my-app/public/favicon.ico         new file:   refs/my-app/public/index.html         new file:   refs/my-app/public/logo192.png         new file:   refs/my-app/public/logo512.png         new file:   refs/my-app/public/manifest.json         new file:   refs/my-app/public/robots.txt         new file:   refs/my-app/src/About.jsx         new file:   refs/my-app/src/Banner.jsx         new file:   refs/my-app/src/Footer.jsx         new file:   refs/my-app/src/Form.jsx         new file:   refs/my-app/src/Front.jsx         new file:   refs/my-app/src/Header.jsx         new file:   refs/my-app/src/Items.jsx         new file:   refs/my-app/src/MyApp.jsx         new file:   refs/my-app/src/Questions.jsx         new file:   refs/my-app/src/Quiz.jsx         new file:   refs/my-app/src/Rootcomp.jsx         new file:   refs/my-app/src/Whatsapp.jsx         new file:   refs/my-app/src/index.css         new file:   refs/my-app/src/index.js         new file:   refs/my-app/src/new.module.css         new file:   refs/my-app/yarn.lock

---
+++ refs/my-app/src/Questions.jsx
@@ -0,0 +1,13 @@
+const Questions = [
+  {
+    id: 1,
+    firstq: "Is Africa The Future?",
+    firsta: "Yes",
+    firstop: false,
+    seconda: "obviously",
+    secondop: false,
+    thirda: "certainly",
+    thirdop: false
+  }
+];
+export default Questions;
---
+++ refs/my-app/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Whatsapp from "./Whatsapp";
+import MyApp from "./MyApp";
+
+ReactDOM.render(<MyApp />, document.getElementById("root"));
